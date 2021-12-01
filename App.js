adf4071241af935c880c5a08d535405a8e38730a
BilalKeb <90609831+BilalKeb@users.noreply.github.com>
2021-12-01 15:05:31 +0100

    First Commit

--- App.js
+++ App.js
@@ -1,66 +1,50 @@
 
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View ,Button} from 'react-native';
+import { Button, StyleSheet, Text, View, ScrollView, Image, Pressable, Alert } from 'react-native';
 
 export default function App() {
-  
+  const createTwoButtonAlert = () => {
+    Alert.alert(
+      "Titre Alerte",
+      "Message Alerte",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "Is Good", onPress: () => console.log("OK Pressed") }
+      ]
+    )
+  }
+
   return (
     <ScrollView style={styles.container}>
-      <View>
-        <Text style={[styles.title, styles.title1]} >Bonjour</Text>
-        <Text style={[styles.title, styles.title2]}>tout</Text>
-        <Text style={[styles.title3, styles.title]}>le monde</Text>
-        <Image source={require('./assets/konexio-logo_1.png')}/>
-        <Image
-          source={{ uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png' }}
-          style={{ width: 50, height: 50 }} />
-        <Button
-          style={styles.button}
-          title="Valider"
-          color="black"
-          onPress={() => Alert.alert('Voilà tout est ok !')}
-          // onPress={() => Linking.openURL(url)}
-        />
-        <Text>Lorem ipsum dolor sit amet, consectetur 
-          adipiscing elit, sed do eiusmod tempor 
-          incididunt ut labore et dolore magna aliqua.
-           Ut enim ad minim veniam, quis nostrud 
-           exercitation ullamco laboris nisi ut 
-           aliquip ex ea commodo consequat. Duis 
-           aute irure dolor in reprehenderit in 
-           voluptate velit esse cillum dolore eu 
-           fugiat nulla pariatur. Excepteur sint
-            occaecat cupidatat non proident, sunt 
-            in culpa qui officia deserunt mollit 
-            anim id est laborum.
-        </Text>
+      <View style={{backgroundColor: "white"}}>
+        <Text style={{fontSize: 30}}>Konexio!</Text>
+        <Text style={{textAlign: "center"}}>Konexio!</Text>
+        <Text style={{fontWeight: "bold"}}>Konexio!</Text>
+        <Image style={styles.img} source={{url:'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png'}} />
+        <Image style={styles.img} source={require('./assets/konexio-logo_1.png')} />
+        <Button title={"2-Button Alert"} onPress={createTwoButtonAlert} />
+        <Pressable onPress={createTwoButtonAlert}>
+          <Text>
+            check
+          </Text>
+        </Pressable>
       </View>
-      <WebView style={{ width: 500, height: 500 }} source={{ uri: 'https://www.konexio.eu/index.html' }} />
+      <StatusBar style="auto" />
     </ScrollView>
   );
 }
 
-
 const styles = StyleSheet.create({
-container: {
-  backgroundColor: 'grey',
-  // height: 500
-},
-title: {
- paddingVertical: 80,
-},
-title1: {
-  fontSize: 30,
-},
-title2: {
-  textAlign: 'center'
-},
-title3: {
-  fontWeight: 'bold',
-},
-button: {
-  backgroundColor: 'yellow'
-}
+  container: {
+    backgroundColor: 'blue',
+    padding: 10,
+  },
+  img: {
+    height: 100,
+  }
 });
-
-export default App;
